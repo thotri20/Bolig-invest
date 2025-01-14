6aa653d557f512f1378261e248a400883e863850
tmktriumf <thotri20@imkatta.no>
2025-01-14 13:03:45 +0100

    add about, projects and contact page

--- common/view/home-view.tsx
+++ common/view/home-view.tsx
@@ -1,28 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import { Home } from "../types/pages/home.types";
 import { urlFor } from "@/sanity/lib/image";
 import PortableText from "../atoms/content/PortableText";
+import Link from "next/link";
 
 
 export default function HomeView({ data }: { data: Home }) {
+  
   return (
     <div>
-      <div className="w-[1920px] h-[110px] relative">
+      <div className="w-[1920px] h-[110px] fixed bg-white top-0">
+        {/* Navbar */}
         <img
           className="w-[250px] h-[70px] left-[136px] top-[24px] absolute"
           src="https://s3-alpha-sig.figma.com/img/e09f/a636/711044123ee6be8d5c796f48f6f5dd42?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Y1em65zPgjGEgkOEK6eIwba2KlE~vdZ3yFyjdPp4~GAYb0RK5lxVcw-a6~yGql5APUSL44CSApLNND3aPS~7QQ7FUxManlqvx0XLzbZeseUqjpKRry0q6-eI3BEHA9iEqL~RWx3pLfADZFCQv6crdq8C2LnEsOnVB37l5w-5RcuHedG19atfBjxmbySmwLSjPASytJj3md6a7LljGL3SrRZHIXfj9UjeWSzDAUbALFv2FzeDY7dKfl8faz8RfDrIqVT60TB881CzW1c-L6AeA2UzY8vjSoOwux8rAzuXO9PyBs-~VREq8eb7Mm~v-GksPjmp0z9T09QEQ6ubzzZHKQ__"
         />
-        <div className="left-[1434px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div 
+        onClick={() => document.getElementById("home")?.scrollIntoView({ behavior: "smooth" })}
+        className="left-[1350px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
           Hjem
         </div>
-        <div className="left-[1537px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div 
+        onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })}
+        className="left-[1450px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
           Om
         </div>
-        <div className="left-[1611px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div 
+        onClick={() => document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" })}
+        className="left-[1530px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
           Prosjekter
         </div>
+        <div className="left-[1700px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+          <Link href="/kontakt">Kontakt</Link>
+        </div>
       </div>
-      <div className="ml-[145px] mt-6 font font-extrabold text-5xl font-['Inter']">
+      <div id="home" className="ml-[145px] mt-[150px] font font-extrabold text-5xl font-['Inter']">
         <p>
           Lorem ipsum dolor sit amet,
           <br />
@@ -30,7 +44,7 @@ export default function HomeView({ data }: { data: Home }) {
         </p>
       </div>
       <div>
-        <div>
+        <div className="flex">
           <Image
             className="ml-[150px] mt-8 rounded-[3px]"
             src={
@@ -40,8 +54,74 @@ export default function HomeView({ data }: { data: Home }) {
             height={800}
             width={800}
           />
+          <div className="w-[646px] h-[155px] text-black text-[32px] font-normal font-['Inter'] ml-[170px] mt-[200px]">
+            Lorem ipsum dolor sit amet, consectetur <br />
+            adipiscing elit, sed do eiusmod tempor i<br />
+            ncididunt ut labore et dolore magna aliqua.
+            <br />
+            Ut enim ad minim veniam, quis nostrud{" "}
+          </div>
         </div>
       </div>
+      <div>
+        <h1 id="about" className="ml-[145px] font font-extrabold text-5xl font-['Inter'] mt-[200px]">
+          Om Oss
+        </h1>
+        <div>
+          <div className="w-[646px] h-[155px] text-black text-[32px] font-normal font-['Inter'] ml-[140px] mt-[75px]">
+            Lorem ipsum dolor sit amet, consectetur <br />
+            adipiscing elit, sed do eiusmod tempor i<br />
+            ncididunt ut labore et dolore magna aliqua.
+            <br />
+            Ut enim ad minim veniam, quis nostrud{" "}
+          </div>
+        </div>
+        <div>
+          <h1 className="ml-[145px] font font-extrabold text-5xl font-['Inter'] mt-[200px]">
+            Våre Ansatte
+          </h1>
+          <div className="flex justify-between mx-[145px] mt-[50px] overflow-x-auto">
+            {/* Box 1 */}
+            <div className="bg-gray-100 w-[300px] p-4 rounded-md shadow-lg text-center flex-shrink-0">
+              <h2 className="text-lg font-bold mb-4">Lorem Ipsum</h2>
+              <div className="h-[300px] w-full bg-gray-300 rounded-md"></div>
+              <p className="mt-2 text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+            {/* Box 2 */}
+            <div className="bg-gray-100 w-[300px] p-4 rounded-md shadow-lg text-center flex-shrink-0">
+              <h2 className="text-lg font-bold mb-4">Lorem Ipsum</h2>
+              <div className="h-[300px] w-full bg-gray-300 rounded-md"></div>
+              <p className="mt-2 text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+            {/* Box 3 */}
+            <div className="bg-gray-100 w-[300px] p-4 rounded-md shadow-lg text-center flex-shrink-0">
+              <h2 className="text-lg font-bold mb-4">Lorem Ipsum</h2>
+              <div className="h-[300px] w-full bg-gray-300 rounded-md"></div>
+              <p className="mt-2 text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+            {/* Box 4 */}
+            <div className="bg-gray-100 w-[300px] p-4 rounded-md shadow-lg text-center flex-shrink-0">
+              <h2 className="text-lg font-bold mb-4">Lorem Ipsum</h2>
+              <div className="h-[300px] w-full bg-gray-300 rounded-md"></div>
+              <p className="mt-2 text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+      <h1 id="projects" className="ml-[145px] font font-extrabold text-5xl font-['Inter'] mt-[200px]">
+          Våre Prosjekter
+        </h1>
+      </div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/8xHNvnZDi7g?si=AKLu26NQgUeV7u_D" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       <div className="bg-neutral-400">
         <h1 className="flex justify-center text-3xl font-semibold text-white">
           {data.title}
@@ -108,6 +188,15 @@ export default function HomeView({ data }: { data: Home }) {
             </div>
           </div>
         ))}
+      </div>
+      <div>
+      <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">
+          &copy; 2025 Bolig Invent AS. All rights reserved.
+        </p>
+      </div>
+    </footer>
       </div>
     </div>
   );
