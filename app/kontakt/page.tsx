2fd4bf15c47587c6e20c9b367731ecc313cde889
tmktriumf <thotri20@imkatta.no>
2025-01-15 11:07:19 +0100

    clean up code, fix video player and add navbar to contact

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -2,16 +2,16 @@
 import React, { useState } from "react";
 import Link from "next/link";
 
-
 const ContactForm: React.FC = () => {
-    
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
@@ -28,100 +28,124 @@ const ContactForm: React.FC = () => {
 
   return (
     <div>
-          <div className="w-[1920px] h-[110px] fixed bg-white top-0">
+      <div className="w-[1920px] h-[110px] fixed bg-white top-0">
         {/* Navbar */}
         <img
           className="w-[250px] h-[70px] left-[136px] top-[24px] absolute"
           src="https://s3-alpha-sig.figma.com/img/e09f/a636/711044123ee6be8d5c796f48f6f5dd42?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Y1em65zPgjGEgkOEK6eIwba2KlE~vdZ3yFyjdPp4~GAYb0RK5lxVcw-a6~yGql5APUSL44CSApLNND3aPS~7QQ7FUxManlqvx0XLzbZeseUqjpKRry0q6-eI3BEHA9iEqL~RWx3pLfADZFCQv6crdq8C2LnEsOnVB37l5w-5RcuHedG19atfBjxmbySmwLSjPASytJj3md6a7LljGL3SrRZHIXfj9UjeWSzDAUbALFv2FzeDY7dKfl8faz8RfDrIqVT60TB881CzW1c-L6AeA2UzY8vjSoOwux8rAzuXO9PyBs-~VREq8eb7Mm~v-GksPjmp0z9T09QEQ6ubzzZHKQ__"
         />
-        <div 
-        onClick={() => document.getElementById("home")?.scrollIntoView({ behavior: "smooth" })}
-        className="left-[1350px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div
+          onClick={() =>
+            document
+              .getElementById("home")
+              ?.scrollIntoView({ behavior: "smooth" })
+          }
+          className="left-[1350px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+        >
           <Link href="/.">Hjem</Link>
         </div>
-        <div 
-        onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })}
-        className="left-[1450px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div
+          onClick={() =>
+            document
+              .getElementById("about")
+              ?.scrollIntoView({ behavior: "smooth" })
+          }
+          className="left-[1450px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+        >
           <Link href="/.">Om</Link>
         </div>
-        <div 
-        onClick={() => document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" })}
-        className="left-[1530px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div
+          onClick={() =>
+            document
+              .getElementById("projects")
+              ?.scrollIntoView({ behavior: "smooth" })
+          }
+          className="left-[1530px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+        >
           <Link href="/.">Prosjekter</Link>
         </div>
         <div className="left-[1700px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
           <Link href="/kontakt">Kontakt</Link>
         </div>
       </div>
-    <div className="flex items-center justify-center min-h-screen">
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-lg bg-white shadow-md rounded-lg p-8"
-      >
-        <h2 className="text-2xl font-bold mb-6 text-gray-800">Kontakt Oss</h2>
-
-        <div className="mb-4">
-          <label
-            htmlFor="name"
-            className="block text-gray-700 font-medium mb-2"
-          >
-            Navn
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-gray-700 font-medium mb-2"
-          >
-            E-post
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-
-        <div className="mb-6">
-          <label
-            htmlFor="message"
-            className="block text-gray-700 font-medium mb-2"
-          >
-            Melding
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-            rows={5}
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          ></textarea>
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-[#04295d] text-white font-medium py-2 px-4 rounded-lg hover:bg-[#04293d] focus:outline-none focus:ring-2 focus:ring-blue-500"
+      <div className="flex items-center justify-center min-h-screen">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-lg bg-white shadow-md rounded-lg p-8"
         >
-          Send melding
-        </button>
-      </form>
-    </div>
+          <h2 className="text-2xl font-bold mb-6 text-gray-800">Kontakt Oss</h2>
+
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Navn
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              E-post
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Melding
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+              rows={5}
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            ></textarea>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-[#04295d] text-white font-medium py-2 px-4 rounded-lg hover:bg-[#04293d] focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Send melding
+          </button>
+        </form>
+      </div>
+      <div>
+        <footer className="bg-gray-800 text-white py-4">
+          <div className="container mx-auto text-center">
+            <p className="text-sm">
+              &copy; 2025 Bolig Invent AS. All rights reserved.
+            </p>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
--- common/view/home-view.tsx
+++ common/view/home-view.tsx
@@ -6,9 +6,7 @@ import { urlFor } from "@/sanity/lib/image";
 import PortableText from "../atoms/content/PortableText";
 import Link from "next/link";
 
-
 export default function HomeView({ data }: { data: Home }) {
-  
   return (
     <div>
       <div className="w-[1920px] h-[110px] fixed bg-white top-0">
@@ -17,26 +15,44 @@ export default function HomeView({ data }: { data: Home }) {
           className="w-[250px] h-[70px] left-[136px] top-[24px] absolute"
           src="https://s3-alpha-sig.figma.com/img/e09f/a636/711044123ee6be8d5c796f48f6f5dd42?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Y1em65zPgjGEgkOEK6eIwba2KlE~vdZ3yFyjdPp4~GAYb0RK5lxVcw-a6~yGql5APUSL44CSApLNND3aPS~7QQ7FUxManlqvx0XLzbZeseUqjpKRry0q6-eI3BEHA9iEqL~RWx3pLfADZFCQv6crdq8C2LnEsOnVB37l5w-5RcuHedG19atfBjxmbySmwLSjPASytJj3md6a7LljGL3SrRZHIXfj9UjeWSzDAUbALFv2FzeDY7dKfl8faz8RfDrIqVT60TB881CzW1c-L6AeA2UzY8vjSoOwux8rAzuXO9PyBs-~VREq8eb7Mm~v-GksPjmp0z9T09QEQ6ubzzZHKQ__"
         />
-        <div 
-        onClick={() => document.getElementById("home")?.scrollIntoView({ behavior: "smooth" })}
-        className="left-[1350px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div
+          onClick={() =>
+            document
+              .getElementById("home")
+              ?.scrollIntoView({ behavior: "smooth" })
+          }
+          className="left-[1350px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+        >
           Hjem
         </div>
-        <div 
-        onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })}
-        className="left-[1450px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div
+          onClick={() =>
+            document
+              .getElementById("about")
+              ?.scrollIntoView({ behavior: "smooth" })
+          }
+          className="left-[1450px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+        >
           Om
         </div>
-        <div 
-        onClick={() => document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" })}
-        className="left-[1530px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+        <div
+          onClick={() =>
+            document
+              .getElementById("projects")
+              ?.scrollIntoView({ behavior: "smooth" })
+          }
+          className="left-[1530px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+        >
           Prosjekter
         </div>
         <div className="left-[1700px] top-[27px] absolute text-black text-3xl font-normal font-['Inter'] hover:text-[#04295d] hover:font-semibold transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
           <Link href="/kontakt">Kontakt</Link>
         </div>
       </div>
-      <div id="home" className="ml-[145px] mt-[150px] font font-extrabold text-5xl font-['Inter']">
+      <div
+        id="home"
+        className="ml-[145px] mt-[150px] font font-extrabold text-5xl font-['Inter']"
+      >
         <p>
           Lorem ipsum dolor sit amet,
           <br />
@@ -64,7 +80,10 @@ export default function HomeView({ data }: { data: Home }) {
         </div>
       </div>
       <div>
-        <h1 id="about" className="ml-[145px] font font-extrabold text-5xl font-['Inter'] mt-[200px]">
+        <h1
+          id="about"
+          className="ml-[145px] font font-extrabold text-5xl font-['Inter'] mt-[200px]"
+        >
           Om Oss
         </h1>
         <div>
@@ -117,17 +136,30 @@ export default function HomeView({ data }: { data: Home }) {
         </div>
       </div>
       <div>
-      <h1 id="projects" className="ml-[145px] font font-extrabold text-5xl font-['Inter'] mt-[200px]">
+        <h1
+          id="projects"
+          className="ml-[145px] font font-extrabold text-5xl font-['Inter'] mt-[200px]"
+        >
           Våre Prosjekter
         </h1>
       </div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/8xHNvnZDi7g?si=AKLu26NQgUeV7u_D" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe
+        width="560"
+        height="315"
+        className="ml-[140px] "
+        src="https://www.youtube.com/embed/8xHNvnZDi7g?si=AKLu26NQgUeV7u_D"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
       <div className="bg-neutral-400">
         <h1 className="flex justify-center text-3xl font-semibold text-white">
           {data.title}
         </h1>
       </div>
-      <div className="flex justify-center mt-6">
+      <div className="flex justify-center">
         <div className="ml-5">
           {data.image?.url && (
             <Image
@@ -168,35 +200,14 @@ export default function HomeView({ data }: { data: Home }) {
           </div>
         ))}
       </div>
-      <div className="flex-wrap flex font-normal justify-evenly text-2xl mt-4">
-        {data.ansatte?.map((ansatte, k) => (
-          <div key={k}>
-            <h2 className="font-semibold">{ansatte.title}</h2>
-            <div>
-              {ansatte.mainImage?.url && (
-                <Image
-                  className="rounded-md"
-                  src={urlFor(ansatte.mainImage.url).url()}
-                  alt={ansatte.mainImage?.alt || "Ansatte Image"}
-                  height={300}
-                  width={300}
-                />
-              )}
-            </div>
-            <div>
-              <PortableText value={ansatte.content} />
-            </div>
-          </div>
-        ))}
-      </div>
       <div>
-      <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto text-center">
-        <p className="text-sm">
-          &copy; 2025 Bolig Invent AS. All rights reserved.
-        </p>
-      </div>
-    </footer>
+        <footer className="bg-gray-800 text-white py-4">
+          <div className="container mx-auto text-center">
+            <p className="text-sm">
+              &copy; 2025 Bolig Invent AS. All rights reserved.
+            </p>
+          </div>
+        </footer>
       </div>
     </div>
   );
